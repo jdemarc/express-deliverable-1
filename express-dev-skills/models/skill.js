703823a10cf3e671d56b1a75189f924381f82fda
joseph <josephjohndemarco@gmail.com>
2020-08-18 22:31:00 -0400

    Refactor model/skill.js

---
+++ express-dev-skills/models/skill.js
@@ -0,0 +1,14 @@
+const todos = [
+    {id: 1, skill: 'HTML'},
+    {id: 2, skill: 'CSS'},
+    {id: 3, skill: 'JavaScript'},
+    {id: 4, skill: 'Express'}
+  ];
+  
+  module.exports = {
+    getAll
+  };
+  
+  function getAll() {
+    return skills;
+  }
