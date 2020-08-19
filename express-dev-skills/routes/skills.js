9e2d96e3038c33fd779bc4844f9bbed00f3e533e
joseph <josephjohndemarco@gmail.com>
2020-08-18 22:34:30 -0400

    Add Skill model requirement.

--- express-dev-skills/routes/skills.js
+++ express-dev-skills/routes/skills.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const Skill = require('../models/skill');
+
+app.get('/skills', function(req, res) {
+  res.render('skills/index', {
+    skills: Skill.getAll()
+  });
 });
 
 module.exports = router;
